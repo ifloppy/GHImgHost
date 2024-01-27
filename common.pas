fc02a1db4bb8f7b465b20de4a7f2fa59748938d0
一张软盘 <68799904+ifloppy@users.noreply.github.com>
2024-01-27 23:52:38 +0800

    File name generator

--- common.pas
+++ common.pas
@@ -5,12 +5,60 @@ unit Common;
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils, DateUtils;
 
 const
   BaseURL = 'https://api.github.com';
+  User_Agent = 'Image Client DEV';
+
+function GeneratePNGFileName(): string;
 
 implementation
 
-end.
+uses main;
 
+function GUIDToStringModified(const GUID: TGUID): string;
+begin
+  SetLength(Result, 38);
+  StrLFmt(PChar(Result), 38,'%.8x-%.4x-%.4x-%.2x%.2x-%.2x%.2x%.2x%.2x%.2x%.2x',
+    [
+     Longint(GUID.D1), GUID.D2, GUID.D3,
+     GUID.D4[0], GUID.D4[1], GUID.D4[2], GUID.D4[3],
+     GUID.D4[4], GUID.D4[5], GUID.D4[6], GUID.D4[7]
+    ]);
+end;
+
+function GeneratePNGFileName(Prefix: string; IdentType: integer): string;
+var
+  dt: TDateTime;
+  ts: TTimeStamp;
+  guid: TGuid;
+begin
+  {
+  yyyymmddhhmmss
+  当前的毫秒时间戳
+  随机生成UUID
+  }
+  case IdentType of
+    0: begin
+      dt := Now;
+      Result := Prefix + FormatDateTime('yyyymmddhhnnss', dt) + '.png';
+    end;
+    1: begin
+      ts := DateTimeToTimeStamp(Now);
+      Result := Prefix + IntToStr(TimeStampToMSecs(ts)) + '.png';
+    end;
+    2: begin
+      CreateGUID(guid);
+      Result := Prefix + GUIDToStringModified(guid) + '.png';
+    end;
+  end;
+end;
+
+function GeneratePNGFileName(): string;
+begin
+  Result := GeneratePNGFileName(Config.ReadString('Formatter', 'FileNamePrefix', ''),
+    Config.ReadInteger('Formatter', 'IndentType', 0));
+end;
+
+end.
