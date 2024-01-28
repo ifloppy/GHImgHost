18ef8a7729b7c18fddd40715648e40d48c6a9d09
一张软盘 <68799904+ifloppy@users.noreply.github.com>
2024-01-28 17:47:01 +0800

    Build

---
+++ build.bat
@@ -0,0 +1,4 @@
+lazbuild --bm=Release --cpu=x86_64 --os=win64 GHImgHost.lpi
+lazbuild --bm=Release --cpu=x86_64 --os=linux GHImgHost.lpi
+lazbuild --bm=Release --cpu=aarch64 --os=win64 GHImgHost.lpi
+lazbuild --bm=Release --cpu=aarch64 --os=linux GHImgHost.lpi
